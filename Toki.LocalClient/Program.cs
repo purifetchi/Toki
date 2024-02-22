c09bed4025e483cc87eec84938e8e1044ea9d2b2
naomiEve <0xlunaric@gmail.com>
2024-02-22 09:08:57 +0100

    feat: add a skeleton for a local client app.
    this will be used for testing the server before we implement
    oauth2.

---
+++ Toki.LocalClient/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Toki.ActivityPub;
+using Toki.ActivityPub.Resolvers;
+using Toki.ActivityStreams.Objects;
+
+var builder = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(svc =>
+        svc.AddActivityPubServices())
+    .Build();
+    
+var resolver = builder.Services.GetRequiredService<ActivityPubResolver>();
+
+// Service test
+var test = await resolver.Fetch<ASActor>(ASObject.Link("https://miku.place/users/prefetcher"));
+Console.WriteLine(test);
