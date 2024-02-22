a4ad51c50079c2b9b1200a29564207e319b4d6fd
naomiEve <0xlunaric@gmail.com>
2024-02-22 09:28:55 +0100

    feat: add a method to localclient for creating a user.

--- Toki.LocalClient/Program.cs
+++ Toki.LocalClient/Program.cs
@@ -3,14 +3,35 @@ using Microsoft.Extensions.Hosting;
 using Toki.ActivityPub;
 using Toki.ActivityPub.Resolvers;
 using Toki.ActivityStreams.Objects;
+using System.CommandLine;
+using Toki.ActivityPub.Persistence.Repositories;
 
 var builder = Host.CreateDefaultBuilder(args)
     .ConfigureServices(svc =>
         svc.AddActivityPubServices())
     .Build();
     
-var resolver = builder.Services.GetRequiredService<ActivityPubResolver>();
+var root = new RootCommand();
 
-// Service test
-var test = await resolver.Fetch<ASActor>(ASObject.Link("https://miku.place/users/prefetcher"));
-Console.WriteLine(test);
+var user = new Command("user", "User-specific commands");
+var userCreate = new Command("create", "Create a user.");
+var username = new Option<string>(
+    name: "--username",
+    description: "The username of the user");
+var password = new Option<string>(
+    name: "--password",
+    description: "The password of the user.");
+
+userCreate.Add(username);
+userCreate.Add(password);
+userCreate.SetHandler(async (usernameValue, passwordValue) =>
+{
+    var svc = builder.Services.GetRequiredService<UserRepository>();
+    var result = await svc.CreateNewUser(usernameValue, passwordValue);
+    Console.WriteLine(result is null ? "Creating user failed." : $"Id: {result.Id}");
+}, username, password);
+
+user.Add(userCreate);
+root.Add(user);
+
+await root.InvokeAsync(args);
