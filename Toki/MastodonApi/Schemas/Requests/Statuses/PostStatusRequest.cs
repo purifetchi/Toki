d3f67b0c0e09e34d338707b573a67d6b27773000
purifetchi <0xlunaric@gmail.com>
2024-03-24 23:55:09 +0100

    fix(mastoapi): bind post status request from form too.
    fixes replying on bloat-fe

--- Toki/MastodonApi/Schemas/Requests/Statuses/PostStatusRequest.cs
+++ Toki/MastodonApi/Schemas/Requests/Statuses/PostStatusRequest.cs
@@ -1,4 +1,5 @@
 using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Mvc;
 using Toki.ActivityPub.Models.Enums;
 
 namespace Toki.MastodonApi.Schemas.Requests.Statuses;
@@ -12,12 +13,14 @@ public record PostStatusRequest
     /// The text content of the status.
     /// </summary>
     [JsonPropertyName("status")]
+    [BindProperty(Name = "status")]
     public string? Status { get; init; }
     
     /// <summary>
     /// Include Attachment IDs to be attached as media.
     /// </summary>
     [JsonPropertyName("media_ids")]
+    [BindProperty(Name = "media_ids")]
     public IReadOnlyList<string>? MediaIds { get; init; }
     
     // TODO: Polls
@@ -26,24 +29,28 @@ public record PostStatusRequest
     /// ID of the status being replied to, if status is a reply.
     /// </summary>
     [JsonPropertyName("in_reply_to_id")]
+    [BindProperty(Name = "in_reply_to_id")]
     public string? InReplyTo { get; init; }
     
     /// <summary>
     /// Mark status and attached media as sensitive?
     /// </summary>
     [JsonPropertyName("sensitive")]
+    [BindProperty(Name = "sensitive")]
     public bool Sensitive { get; init; }
     
     /// <summary>
     /// Text to be shown as a warning or subject before the actual content.
     /// </summary>
     [JsonPropertyName("spoiler_text")]
+    [BindProperty(Name = "spoiler_text")]
     public string? SpoilerText { get; init; }
     
     /// <summary>
     /// Sets the visibility of the posted status.
     /// </summary>
     [JsonPropertyName("visibility")]
+    [BindProperty(Name = "visibility")]
     public string? Visibility { get; init; }
     
     /// <summary>
