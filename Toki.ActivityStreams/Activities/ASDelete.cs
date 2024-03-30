4c915f891c736a3e9401826a5f56020122f69ac6
purifetchi <0xlunaric@gmail.com>
2024-03-30 13:13:28 +0100

    feat(activitystreams): add a definition for ASDelete.

---
+++ Toki.ActivityStreams/Activities/ASDelete.cs
@@ -0,0 +1,16 @@
+namespace Toki.ActivityStreams.Activities;
+
+/// <summary>
+/// An ActivityStreams delete activity.
+/// </summary>
+public class ASDelete : ASActivity
+{
+    /// <summary>
+    /// Constructs a new ASDelete activity.
+    /// </summary>
+    public ASDelete()
+        : base("Delete")
+    {
+        
+    }
+}
