69a48c5c81c9a705dce2786ca164c7e66974f121
purifetchi <0xlunaric@gmail.com>
2024-04-25 19:04:16 +0200

    feat(activitypub+mastoapi): paginate bookmarks.

--- Toki/Controllers/MastodonApi/Accounts/BookmarksController.cs
+++ Toki/Controllers/MastodonApi/Accounts/BookmarksController.cs
@@ -1,10 +1,12 @@
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Toki.ActivityPub.Models;
 using Toki.ActivityPub.Persistence.Repositories;
 using Toki.Extensions;
 using Toki.MastodonApi.Renderers;
 using Toki.MastodonApi.Schemas.Objects;
+using Toki.MastodonApi.Schemas.Params;
 using Toki.Middleware.OAuth2;
 using Toki.Middleware.OAuth2.Extensions;
 
@@ -27,25 +29,27 @@ public class BookmarksController(
     [HttpGet]
     [OAuth("read:bookmarks")]
     [Produces("application/json")]
-    public async Task<ActionResult<IEnumerable<Status>>> GetBookmarks()
+    public async Task<ActionResult<IEnumerable<Status>>> GetBookmarks(
+        [FromQuery] PaginationParams paginationParams)
     {
         var user = HttpContext.GetOAuthToken()!
             .User;
 
         // TODO: We really need to unify adding post rendering data.
-        // TODO: Pagination.
-        var query = await postRepo.CreateCustomBookmarkedPostQuery()
-            .Where(b => b.UserId == user.Id)
-            .Include(b => b.Post)
-            .Include(b => b.Post.Author)
-            .Include(b => b.Post.Attachments)
-            .Include(b => b.Post.Parent)
-            .Include(b => b.Post.Parent!.Author)
-            .OrderByDescending(b => b.Id)
-            .Select(b => b.Post)
-            .ToListAsync();
+        var bookmarksView = postRepo.GetBookmarksForUser(user);
+        
+        if (paginationParams.MaxId is not null)
+            bookmarksView = bookmarksView.Before(paginationParams.MaxId.Value);
+
+        if (paginationParams.SinceId is not null)
+            bookmarksView = bookmarksView.After(paginationParams.SinceId.Value);
+
+        var bookmarks = await bookmarksView
+            .Limit(paginationParams.Limit)
+            .Project<Post>(b => b.Post)
+            .GetWithMastodonPagination(HttpContext);
 
         return Ok(
-            await statusRenderer.RenderManyStatusesForUser(user, query));
+            await statusRenderer.RenderManyStatusesForUser(user, bookmarks));
     }
 }
