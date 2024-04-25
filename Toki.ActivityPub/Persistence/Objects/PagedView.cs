b4c41032f0a16034c1b9a747a85bcfc76a09c493
naomiEve <0xlunaric@gmail.com>
2024-04-25 12:08:49 +0200

    feat(activitypub): add a PagedView<TModel> class.
    this will be returned from repositories returning a list of items.

---
+++ Toki.ActivityPub/Persistence/Objects/PagedView.cs
@@ -0,0 +1,45 @@
+using Microsoft.EntityFrameworkCore;
+using Toki.ActivityPub.Models;
+
+namespace Toki.ActivityPub.Persistence.Objects;
+
+/// <summary>
+/// A paged view into a database.
+/// </summary>
+/// <typeparam name="TModel">The type of the model.</typeparam>
+public class PagedView<TModel>(IQueryable<TModel> query)
+    where TModel : AbstractModel
+{
+    /// <summary>
+    /// Paginates the view.
+    /// </summary>
+    /// <param name="since">Since what id should we paginate.</param>
+    /// <returns>This.</returns>
+    public PagedView<TModel> Paginate(
+        Ulid since)
+    {
+        query = query.Where(m => m.Id.CompareTo(since) == -1);
+        return this;
+    }
+
+    /// <summary>
+    /// Limits the amount of objects we can fetch.
+    /// </summary>
+    /// <param name="count">The count.</param>
+    /// <returns>This.</returns>
+    public PagedView<TModel> Limit(
+        int count)
+    {
+        query = query.Take(count);
+        return this;
+    }
+
+    /// <summary>
+    /// Gets the view as a list.
+    /// </summary>
+    /// <returns>The list.</returns>
+    public async Task<List<TModel>> ToList()
+    {
+        return await query.ToListAsync();
+    }
+}
